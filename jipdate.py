f8776b27621c36abaa5e5a2ebeca3d04c011a8ca
Joakim Bech <joakim.bech@linaro.org>
2017-05-09 21:28:54 +0200

    Automatically populate the status_update.txt file
    Add a new parameter '-c' that will gather all open issues belonging to a
    the user running the script. This will also automatically create a
    status_update.txt containing all issues found.
    
    Signed-off-by: Joakim Bech <joakim.bech@linaro.org>

--- jipdate.py
+++ jipdate.py
@@ -5,6 +5,7 @@ import json
 import re
 import os
 import sys
+from argparse import ArgumentParser
 
 # Sandbox server
 server = 'https://dev-projects.linaro.org'
@@ -12,56 +13,112 @@ server = 'https://dev-projects.linaro.org'
 # Production server, comment out this in case you want to use the real server
 #server = 'https://projects.linaro.org'
 
-try:
-    username = os.environ['JIRA_USERNAME']
-    password = os.environ['JIRA_PASSWORD']
-except KeyError:
-    print "Forgot to export JIRA_USERNAME and JIRA_PASSWORD?"
-    sys.exit()
+def get_args():
+    parser = ArgumentParser(description='Script to create images on a format \
+                        that Mynewts bootloader expects')
 
-credentials=(username, password)
+    parser.add_argument('-c', required=False, action="store_true", \
+            default=False, \
+            help='Gather all Jira issue(s) assigned to you into the \
+            status_update.txt file')
 
-jira = JIRA('https://projects.linaro.org', basic_auth=credentials)
-jira = JIRA(server, basic_auth=credentials)
+    return parser.parse_args()
 
-# Regexp to match Jira issue on a single line, i.e:
-# [SWG-28]
-# [LITE-32]
-# etc ...
-regex = r"^\[[A-Z]+-\d+\]\n$"
+################################################################################
 
-# Contains the status text, it could be a file or a status email
-status = ""
+def get_my_name():
+    n = os.environ['JIRA_USERNAME'].split("@")[0].title()
+    return n.replace(".", " ")
 
-with open("status_update.txt") as f:
-    status = f.readlines()
+################################################################################
 
-myissue = "";
-mycomment = "";
-
-def update_jira(i, c):
+def update_jira(jira, i, c):
     print "Updating Jira issue: %s with comment:" % i
     print "-- 8< --------------------------------------------------------------------------"
     print "%s" % c
     print "-- >8 --------------------------------------------------------------------------\n\n"
     jira.add_comment(i, c)
 
-# State to keep track of whether we are in an issue or a comment
-state = "issue"
+################################################################################
 
-for line in status:
-    # New issue?
-    if re.search(regex, line):
-        if state == "comment":
-            update_jira(myissue, mycomment)
-            state = "issue"
+message_header = """Hi,
 
-        myissue = line.strip();
-        myissue = myissue[1:-1]
-        mycomment = ""
-        state = "comment"
-    else:
-        mycomment += line
+This is the status update from me for the last week.
 
-if len(mycomment) > 0:
-    update_jira(myissue, mycomment)
+Cheers!
+"""
+
+def get_jira_issues(jira):
+    jql = "assignee = currentUser() AND status not in (Resolved, Closed)"
+    my_issues = jira.search_issues(jql)
+    msg = message_header + get_my_name() + "\n\n"
+
+    with open ("status_update.txt", "w") as f:
+        f.write(msg)
+
+        print "Found issue:"
+        for issue in my_issues:
+            print "%s : %s" % (issue, issue.fields.summary)
+            f.write("[%s]\nREMOVE THIS LINE: %s\nNo updates since last week.\n\n" % (issue,
+                issue.fields.summary))
+
+    print "\nstatus_update.txt has been prepared with all of your open\n" + \
+          "issues. Manually edit the file, then re-run this script without\n" + \
+          "the '-c' parameter to update your issues."
+
+################################################################################
+def main(argv):
+    args = get_args()
+    try:
+        username = os.environ['JIRA_USERNAME']
+        password = os.environ['JIRA_PASSWORD']
+    except KeyError:
+        print "Forgot to export JIRA_USERNAME and JIRA_PASSWORD?"
+        sys.exit()
+
+    credentials=(username, password)
+    jira = JIRA(server, basic_auth=credentials)
+
+    if args.c:
+        get_jira_issues(jira)
+        sys.exit()
+
+    # Regexp to match Jira issue on a single line, i.e:
+    # [SWG-28]
+    # [LITE-32]
+    # etc ...
+    regex = r"^\[[A-Z]+-\d+\]\n$"
+
+    # Contains the status text, it could be a file or a status email
+    status = ""
+
+    with open("status_update.txt") as f:
+        status = f.readlines()
+
+    myissue = "";
+    mycomment = "";
+
+    # State to keep track of whether we are in an issue or a comment
+    state = "issue"
+
+    for line in status:
+        # New issue?
+        if re.search(regex, line):
+            if state == "comment":
+                update_jira(jira, myissue, mycomment)
+                state = "issue"
+
+            myissue = line.strip();
+            myissue = myissue[1:-1]
+            mycomment = ""
+            state = "comment"
+        else:
+            mycomment += line
+
+    if len(mycomment) > 0:
+        update_jira(jira, myissue, mycomment)
+
+    print "Successfully updated your Jira tickets!"
+
+if __name__ == "__main__":
+        main(sys.argv)
