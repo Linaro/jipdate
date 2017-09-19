fbb6da9c22ce6cae18d774665c2344daaa02090d
Joakim Bech <joakim.bech@linaro.org>
2017-09-19 09:05:40 +0200

    Make if possible to store config at system default path
    - Introduce a new default config filename: ".jipdate.yml". For backwards
      compatibility, the old "config.yml" name is still working.
    
    - Enable the user to store the config file in either:
        * jipdate.py script directory
        * $HOME/
        * $HOME/.config/jipdate/
    
    When running the script it searches for a config file in the same order
    as above, but if no config is found, then it will create this file:
        $HOME/.config/jipdate/.jipdate.yml
    
    Signed-off-by: Joakim Bech <joakim.bech@linaro.org>

--- cfg.py
+++ cfg.py
@@ -1,7 +1,26 @@
+import os
+import sys
+
 TEST_SERVER = 'https://dev-projects.linaro.org'
 PRODUCTION_SERVER = 'https://projects.linaro.org'
+server = PRODUCTION_SERVER
 
 args = None
-config_filename = "config.yml"
-server = PRODUCTION_SERVER
+
+# Config file paths and name, basically we allow the user to store the
+# .config.yml/config.yml in either the application directory, the $HOME
+# directory or the  $HOME/.config/jipdate directory.
+config_app_dir = sys.path[0]
+config_home_config_dir = os.environ['HOME'] + "/.config/jipdate"
+config_home_dir = os.environ['HOME']
+config_locations = [config_app_dir, config_home_dir, config_home_config_dir]
+config_path = config_home_config_dir
+
+# Config filenames
+config_filename = ".jipdate.yml"
+config_legacy_filename = "config.yml"
+
+# Config current file, will be set when initiate the config
+config_file = None
+
 yml_config = None
