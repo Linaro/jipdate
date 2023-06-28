Linaro/jipdate

commit e865f7bbe52385fbe854a36fa5f9dee96a217e57

jipcreate: complement '-v' with '--verbose' flag
For verbose output.

Signed-off-by: Anders Roxell <anders.roxell@linaro.org>

jipdate/jipcreate.py
@@ -44,6 +44,7 @@ def get_parser():
 
     parser.add_argument(
         "-v",
+        "--verbose",
         required=False,
         action="store_true",
         default=False,

commit a7a8db1fa3a46c8bd853409ed92dc7adbc197458

jipcreate: fix verbose variable
Fixes: e865f7bbe523 ("jipcreate: complement '-v' with '--verbose' flag")
Signed-off-by: Anders Roxell <anders.roxell@linaro.org>

jipdate/jipcreate.py
@@ -86,7 +86,7 @@ def parse_issue_file(new_issue_file):
 def initialize_logger(args):
     LOG_FMT = "[%(levelname)s] %(funcName)s():%(lineno)d   %(message)s"
     lvl = log.ERROR
-    if args.v:
+    if args.verbose:
         lvl = log.DEBUG
 
     log.basicConfig(