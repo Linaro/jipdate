Linaro/jipdate

commit a38bac45060bcd13d06cedae36b7f967d1cd113d

jipsearch: complement '-v' with '--verbose' flag
For verbose output.

Signed-off-by: Anders Roxell <anders.roxell@linaro.org>

jipdate/jipsearch.py
@@ -179,6 +179,7 @@ def get_parser():
 
     parser.add_argument(
         "-v",
+        "--verbose",
         required=False,
         action="store_true",
         default=False,

commit f1b588b5dc03bb1193d13c78e807baaa984a3d25

jipsearch: fix verbose variable
Fixes: a38bac45060b ("jipsearch: complement '-v' with '--verbose' flag")
Signed-off-by: Anders Roxell <anders.roxell@linaro.org>

jipdate/jipsearch.py
@@ -206,7 +206,7 @@ def get_parser():
 def initialize_logger(args):
     LOG_FMT = "[%(levelname)s] %(funcName)s():%(lineno)d   %(message)s"
     lvl = log.ERROR
-    if args.v:
+    if args.verbose:
         lvl = log.DEBUG
 
     log.basicConfig(