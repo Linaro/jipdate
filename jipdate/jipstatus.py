Linaro/jipdate

commit a4ec0150f45eb1eb6eadd41f8614f2b86793ec81

jipstatus: complement '-v' with '--verbose' flag
For verbose output.

Signed-off-by: Anders Roxell <anders.roxell@linaro.org>

jipdate/jipstatus.py
@@ -214,6 +214,7 @@ def get_parser():
 
     parser.add_argument(
         "-v",
+        "--verbose",
         required=False,
         action="store_true",
         default=False,

commit 953e327fd2080045c80813dfa6a2747348d924c1

jipstatus: fix verbose variable
Fixes: a4ec0150f45e ("jipstatus: complement '-v' with '--verbose' flag")
Signed-off-by: Anders Roxell <anders.roxell@linaro.org>

jipdate/jipstatus.py
@@ -232,7 +232,7 @@ def get_parser():
 def initialize_logger(args):
     LOG_FMT = "[%(levelname)s] %(funcName)s():%(lineno)d   %(message)s"
     lvl = log.ERROR
-    if args.v:
+    if args.verbose:
         lvl = log.DEBUG
 
     log.basicConfig(